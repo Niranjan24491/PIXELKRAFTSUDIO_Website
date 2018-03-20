901825c10cf4ae76ca8c366b5904bab77d5a3ffa
Niranjan <nthrineshwar@deloitte.com>
2018-03-20 15:36:22 +0530

    updated initial code

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,91 @@
-const path = require('path');
- 
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const webpack = require("webpack");
+
 module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: [
-    './main.jsx',
-  ],
+  entry: {
+    app: "./src/main.jsx"
+  },
+  devtool: "inline-source-map",
   output: {
-    path: path.join(__dirname, 'www'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, "www"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.jsx$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
+        use: ["babel-loader"],
+        include: [path.resolve(__dirname, "src")]
       },
-    ],
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ],
+        include: [path.resolve(__dirname, "www")]
+      },
+      {
+        test: /\.(gif|png|jpg|svg)$/i,
+        use: [
+          "url-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 50
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: "60-90",
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 50
+              }
+            }
+          }
+        ]
+      }
+    ]
   },
+  devServer: {
+    contentBase: path.join(__dirname, "src")
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".scss"]
+  },
+  plugins: [
+    new CleanWebpackPlugin(["www"]),
+    new webpack.DefinePlugin({
+      // <-- key to reducing React's size
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.jsx$|\.css$|\.html$|\.jpg$|\.png$|\.js$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
 };
